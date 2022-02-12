bb2d6d0cd216421926981b0ad493075a12d47d4a
Andrey <chertiav@gmail.com>
2022-02-12 13:13:06 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,57 @@
+// alert('I like JavaScript');
+// let tree;
+
+// первая часть
+let caset = 20;
+const newTree = 12;
+let str = 'Stiga';
+let logic = true;
+let nullable = null;
+// console.log(nullable);
+// console.log(typeof nullable);
+
+// вторая часть
+// преобразования числа получаемого через форму в число
+
+// let fromHTML = Number (prompt('Enter a number'));
+// console.log ( typeof fromHTML)
+
+// третья часть условная конструкция 
+
+// if(typeof str === 'string'){
+//     console.log('It string');
+// }
+
+// if(typeof logic === 'string'){
+//     console.log('It string');
+//     // console.log('It string');
+// }else if(typeof logic === 'boolean'){
+//     console.log('It boolean');
+// }else {
+// }
+
+
+
+// Условный (тернарный) оператор
+
+// условие ? выражение1 : выражение2
+// условие
+// Выражение, принимающее значение true или false.
+// выражение1, выражение2
+// Выражения, значения которых могут принадлежать любому типу. 
+
+
+// let result = (caset >25) ? 17 : (caset === 20) ? 'true' : 'false';
+// console.log(result)
+
+
+let one = 10;
+let two = 20;
+let three =30;
+
+
+let result = (one >11) ? 'one' : (two === 30) ? 'two' : (three === 30) ? 'three' : 'false';
+console.log(result)
+
+
+
