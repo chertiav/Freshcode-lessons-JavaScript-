adeef84aa722b8971db5307ee1d53cca43ef7136
Andrey <chertiav@gmail.com>
2022-02-12 14:08:03 +0200

    add switch

--- src/index.js
+++ src/index.js
@@ -45,13 +45,77 @@ let nullable = null;
 // console.log(result)
 
 
-let one = 10;
-let two = 20;
-let three =30;
+// let one = 10;
+// let two = 20;
+// let three = 30;
 
 
-let result = (one >11) ? 'one' : (two === 30) ? 'two' : (three === 30) ? 'three' : 'false';
-console.log(result)
+// let result = (one > 8) ? 'one' : (two === 10) ? 'two' : (three === 30) ? 'three' : 'false';
+// console.log(result);
+
+
+
+// switch
+
+const sum = function(a, b){
+    return a + b;
+}
+
+const sub = function(a, b){
+    return a - b;
+}
+
+const div = function(a, b){
+    return a / b;
+}
+
+const mul = function(a, b){
+    return a * b;
+}
+
+const calculate = function(num1, num2, mathOperation){
+    if (isNaN(num1-num2)){
+        return 'Enter number, please'
+    }
+
+let operation;
+
+switch (mathOperation) {
+    case '+' : {
+        operation = sum;
+        break;
+    }
+    case '-' : {
+        operation = sub;
+        break;
+    }
+    case '/' : {
+        operation = div;
+        break;
+    }
+    case '*' : {
+        operation = mul;
+        break;
+    }
+    default: {
+        console.log('Unknown math operation')
+    }
+}
+if(typeof operation === 'function'){
+    return operation (num1, num2);
+} else{
+    return 'Unknown math operation';
+}
+
+}
+
+const userInp1 = Number(prompt('Enter first number'));
+const userInp2 = Number(prompt('Enter first number'));
+const mathOperation = prompt('Enter math operation');
+
+const result = calculate(userInp1, userInp2, mathOperation);
+console.log(result);
+
 
 
 
